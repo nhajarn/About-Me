107998d9815cdfeb06746f2fb292d9c743411b1f
Hajar Alruqi <alruqi.hajar@gmail.com>
2024-01-31 10:35:04 +0300

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Hajar on 19/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by Hajar on 19/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
